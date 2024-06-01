2f53239edf66325e999640ef37dc5c86789e1a42
Albina-K <kamalieva.albish@mail.ru>
2024-06-01 21:37:36 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+            //LINQ выражение
+            var selectedPeople = from p in people // промежуточная переменная p 
+                                 where p.StartsWith("A") // фильтрация по условию
+                                 orderby p // сортировка по возрастанию (дефолтная)
+                                 select p; // выбираем объект и сохраняем в выборку
+
+            foreach (string s in selectedPeople)
+            {
+                Console.WriteLine(s);
+            }
+
+            /*
+
+            from variable in collection // каждую переменную в коллекции
+            where condition // для которой выполняется условие
+            select variable; // добавить в выборку
+
+            */
+        }
+        //метод расширения
+        static void Main1(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var selectedPeople = people.Where(p => p.StartsWith("А")).OrderBy(p => p);
+
+            foreach (string s in selectedPeople)
+                Console.WriteLine(s);
+        }
+
+        static void Main2(string[] args)
+        {
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            var selectedPeople = (from p in people
+                                  where p.StartsWith("A")
+                                  orderby p
+                                  select p).Count();
+            Console.WriteLine($"В выборке {selectedPeople} чел");
+        }
+
+        static void Main3(string[] args)
+        {
+            var objects = new List<object>()
+            {
+                1,
+               "Сергей ",
+               "Андрей ",
+               300,
+            };
+
+            var names = from a in objects
+                        where a is string // проверка на совместимость с типом
+                        orderby a // сортировка по имени
+                        select a; // выборка
+
+            foreach (var name in names)
+                Console.WriteLine(name);
+
+            foreach (var namess in objects.Where(a => a is string).OrderBy(a=>a))
+                Console.WriteLine(namess);
+        }
+    }
+}
+
+
---
+++ Module14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //выбрать имена на букву А и отсортировать в алфавитном порядке.
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            // список, куда будем сохранять выборку
+            var orderedList = new List<string>();
+
+            // пробежимся по массиву и добавим искомое в наш список
+            foreach (string person in people)
+            {
+                if (person.ToUpper().StartsWith("А"))
+                    orderedList.Add(person);
+            }
+
+            // отсортируем список
+            orderedList.Sort();
+            foreach (string s in orderedList)
+                Console.WriteLine(s);
+        }
+    }
+}
