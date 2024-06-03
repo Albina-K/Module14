9eff5bdfacf16e947a6acb05b7f9f3eafd2c2fa8
Albina <kamalieva.albish@mail.ru>
2024-06-03 18:43:40 +0300

    process

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -70,6 +70,126 @@ namespace LinqTest
             foreach (var namess in objects.Where(a => a is string).OrderBy(a=>a))
                 Console.WriteLine(namess);
         }
+        //Рассмотрим пример, где надо выбрать четные числа больше 10:
+        static void Main4(string[] args)
+        {
+            int[] numbers =
+            {
+                1,
+                2,
+                3,
+                4,
+                10,
+                34,
+                55,
+                66,
+                77,
+                88
+            };
+
+            var evenNums = from i in numbers
+                           where i % 2 == 0 && i > 10
+                           select i;
+
+            foreach (int i in evenNums)
+                Console.WriteLine(i);
+        }
+
+        static void Main5(string[] args)
+        {
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+            var bigCities = from russianCity in russianCities
+                            where russianCity.Population < 1000000
+                            orderby russianCity.Population descending
+                            select russianCity;
+
+            foreach (var bigCity in bigCities)
+                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+
+            //То же самое с помощью методов расширения выглядит ещё проще: 
+            var bigCities2 = russianCities.Where(c => c.Population > 1000000)
+                .OrderByDescending(c => c.Population);
+
+            var bigCities3 = russianCities.Where(c => c.Name.Length <= 10)
+                .OrderBy (c => c.Name.Length);
+        }
+
+        // Создадим модель класс для города
+        public class City    
+        {
+            public City(string name, long population) 
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
+        }
+
+        static void Main6(string[] args)
+        {
+            // Словарь для хранения стран с городами
+            var Countries = new Dictionary<string, List<City>>();
+
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+            Countries.Add("Россия", russianCities);
+
+            // Добавим Беларусь
+            var belarusCities = new List<City>();
+            belarusCities.Add(new City("Минск", 1200000));
+            belarusCities.Add(new City("Витебск", 362466));
+            belarusCities.Add(new City("Гродно", 368710));
+            Countries.Add("Беларусь", belarusCities);
+
+            // Добавим США
+            var americanCities = new List<City>();
+            americanCities.Add(new City("Нью-Йорк", 8399000));
+            americanCities.Add(new City("Вашингтон", 705749));
+            americanCities.Add(new City("Альбукерке", 560218));
+            Countries.Add("США", americanCities);
+
+            // сделать выборку городов - миллионников по всем странам.
+            var cities = from country in Countries // пройдемся по странам
+                         from city in country.Value // пройдемся по городам
+                         where city.Population > 1000000 // выберем города-миллионники
+                         orderby city.Population descending // отсортируем по населению
+                         select city;
+
+            foreach (var city in cities)
+                Console.WriteLine(city.Name + " - " + city.Population);
+        }
+        //Соедините все слова в одну последовательность(каждое слово — отдельный элемент последовательности).
+        static void main7(string[] args)
+        {
+            string[] text =
+            {
+                "Раз два три",
+                "четыре пять шесть",
+                "семь восемь девять"
+            };
+
+            var words = from str in text // пробегаемся по элементам массива
+                        from word in str.Split(' ') // дробим каждый элемент по пробелам, сохраняя в новую последовательность
+                        select word; // собираем все куски в результирующую выборку
+
+            // выводим результат
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
     }
 }
 
---
+++ SelectMany/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SelectMany
+{//SelectMany — метод расширения, предоставляющий другой способ сделать сложную фильтрацию. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Подготовим данные
+            List<Student> students = new List<Student>
+            {
+                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+            // Составим запрос ()
+            var selectedStudents = students.SelectMany(
+                // коллекция, которую нужно преобразовать
+                s => s.Languages,
+                // функция преобразования, применяющаяся к каждому элементу коллекции
+                (s, l) => new { Student = s, Lang = l })
+                // дополнительные условия            
+                .Where(s => s.Lang == "английский" && s.Student.Age < 28)
+                // указатель на объект выборки
+                .Select(s => s.Student);
+
+            // Выведем результат
+            foreach(Student student in selectedStudents)
+            {
+                Console.WriteLine($"{student.Name} - {student.Age}");
+            }
+        }
+
+        //Сделайте выборку тех, которые производят мобильную технику.
+        static void Main2(string[] args)
+        {
+            var companies = new Dictionary<string, string[]>();
+
+            companies.Add("Apple", new[] { "Mobile", "Desktop" });
+            companies.Add("Samsung", new[] { "Mobile" });
+            companies.Add("IBM", new[] { "Desktop" });
+
+            var mobileCompanies = companies
+                // смотрим те из выборки, значения в которых содержат искомое
+                .Where(c => c.Value.Contains("Mobile"));
+
+            foreach (var company in mobileCompanies)
+                Console.WriteLine(company.Key);
+        }
+        //Сделайте выборку всех чисел в новую коллекцию, расположив их в ней по возрастанию.
+        static void Main3(string[] args)
+        {
+            var numList = new List<int[]>()
+            {
+                new[] {2, 3, 7, 1},
+                new[] {45, 17, 88, 0},
+                new[] {23, 32, 44, -6},
+            };
+
+            var collection = numList
+                .SelectMany(c => c) // выбираем элементы
+                .OrderBy(c => c); // сортируем
+            // выводим
+            foreach (var ord in collection)
+                Console.WriteLine(ord);
+        }
+    }
+}
